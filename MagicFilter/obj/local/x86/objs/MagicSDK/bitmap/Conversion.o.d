ace9bdcb4e29410e627d8d0ddf0aaa680ce8ab7a
wuhaoyu1990 <wuhaoyu1990@qq.com>
2016-01-14 21:56:46 +0800

    fix bug

---
+++ MagicFilter/obj/local/x86/objs/MagicSDK/bitmap/Conversion.o.d
@@ -0,0 +1,67 @@
+obj/local/x86/objs/MagicSDK/bitmap/Conversion.o: \
+ jni/bitmap/Conversion.cpp jni/bitmap/Conversion.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/stdio.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs_elf.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/api-level.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/stdint.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/machine/wchar_limits.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/int-ll64.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/bitsperlong.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/bitsperlong.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/stddef.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/compiler.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/posix_types_32.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/sysmacros.h \
+ D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/log.h
+
+jni/bitmap/Conversion.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/stdio.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs_elf.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/api-level.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/stdint.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/machine/wchar_limits.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/int-ll64.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/bitsperlong.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/bitsperlong.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/stddef.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/linux/compiler.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm/posix_types_32.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/asm-generic/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/sysmacros.h:
+
+D:/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/log.h:
